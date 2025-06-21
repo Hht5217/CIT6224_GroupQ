7123c8846b08954b895b527b7e900aa7fe599d46
Hht5217 <123789114+Hht5217@users.noreply.github.com>
2025-06-22 04:01:57 +0800

    minor fix

--- includes/timeout.php
+++ includes/timeout.php
@@ -1,17 +1,19 @@
 <?php
 $timeout_duration = 300; // 5 minutes timeout
 
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
-    // Get the current directory
-    $current_dir = dirname($_SERVER['SCRIPT_NAME']);
+if (isset($_SESSION['user_id'])) {
+    if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
+        // Get the current directory
+        $current_dir = dirname($_SERVER['SCRIPT_NAME']);
 
-    // If in /admin or a subdirectory, go up one level for logout.php
-    if (strpos($current_dir, '/admin') !== false) {
-        header("Location: ../logout.php?timeout=1");
-    } else {
-        header("Location: logout.php?timeout=1");
+        // If in /admin or a subdirectory, go up one level for logout.php
+        if (strpos($current_dir, '/admin') !== false) {
+            header("Location: ../logout.php?timeout=1");
+        } else {
+            header("Location: logout.php?timeout=1");
+        }
+        exit;
     }
-    exit;
+    $_SESSION['LAST_ACTIVITY'] = time();
 }
-$_SESSION['LAST_ACTIVITY'] = time();
 ?>
